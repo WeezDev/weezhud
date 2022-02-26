e02ae27b36aab29abef1032fccf5a1646dd59756
Weez-Dev <75889224+Weez-Dev@users.noreply.github.com>
2022-02-26 16:47:40 +0100

    Add files via upload

---
+++ wzHud/fxmanifest.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+---------- By Weez#8876 -----------
+------- Weez Developpement --------
+-- https://discord.gg/BK7hTKub8e --
+-----------------------------------
+
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts {
+    "wz_cl.lua",
+}
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/css/style.css',
+	'html/css/grid.css',
+	'html/js/main.js',
+	'html/img/*',
+	'html/fonts/*'
+}
